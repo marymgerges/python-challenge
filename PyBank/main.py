66ec173a45b42dd456ec8252774154949ec7d8a2
marymgerges <marymgerges@gmail.com>
2023-12-08 17:58:41 -0800

    update python-challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,85 @@
+# Modules
+import os
+import csv
 
+# Set path for file
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
+
+# Set initial variable for total number of months included in the data
+total_number_months = 0
+
+# Set initial variable for total profit/loss over the entire period
+total_profit_or_loss = 0
+
+# Set initial variable for the previous profit/loss
+previous_profit_or_loss = 0
+
+# Set initial variable for the change in profit/loss over the entire period
+change_of_profit_or_loss = 0
+
+# Set initial variable for the greatest increase in profits over the entire period
+greatest_increase_profits = ["", 0]
+
+# Set initial variable for the greatest decrease in profits over the entire period
+greatest_decrease_profits = ["", 99999999]
+
+# Read in the CSV file
+with open(budget_data_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Skip row of headers
+    header = next(csvreader)
+
+    # Loop through the data
+    for row in csvreader:
+
+        # Calculate total number of months in data
+        total_number_months +=1
+
+        # Calculate total profit/loss over entire period
+        total_profit_or_loss += int(row[1])
+
+        # Calculate change in profit/loss from previous to current row
+        change_of_profit_or_loss = int(row[1]) - previous_profit_or_loss
+        previous_profit_or_loss = int(row[1])
+
+        # Calculate greatest increase in profits (date and amount) over entire period
+        if greatest_increase_profits[1] < change_of_profit_or_loss:
+            greatest_increase_profits[0] = row[0]
+            greatest_increase_profits[1] = change_of_profit_or_loss
+
+        # Calculate greatest decrease in profits (date and amount) over entire period
+        if greatest_decrease_profits[1] > change_of_profit_or_loss:
+            greatest_decrease_profits[0] = row[0]
+            greatest_decrease_profits[1] = change_of_profit_or_loss 
+
+# Calculate average change in profit/loss over entire period and set value to two decimal places
+average_change_profits_or_losses = round(change_of_profit_or_loss/total_number_months, 2)
+
+
+# Print analysis to console
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_number_months}")
+print(f"Total: ${total_profit_or_loss}")
+print(f"Average Change: ${average_change_profits_or_losses}")
+print(f"Greatest Increase in Profits: {greatest_increase_profits[0]} (${greatest_increase_profits[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_profits[0]} (${greatest_decrease_profits[1]})")
+
+analysis = os.path.join("analysis/analysis_results.txt")
+
+# Print analysis to text file
+with open(analysis, "w") as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow("Financial Analysis")
+    csvwriter.writerow("----------------------------")
+    csvwriter.writerow(f"Total Months: {total_number_months}")
+    csvwriter.writerow(f"Total: ${total_profit_or_loss}\n")
+    csvwriter.writerow(f"Average Change: ${average_change_profits_or_losses:2f}")
+    csvwriter.writerow(f"Greatest Increase in Profits: {greatest_increase_profits[0]} (${greatest_increase_profits[1]})")
+    csvwriter.writerow(f"Greatest Decrease in Profits: {greatest_decrease_profits[0]} (${greatest_decrease_profits[1]})")
+
+# Print analysis to terminal
+print(analysis)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,84 @@
+# Modules
+import os
+import csv
 
+# Set path for file
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+# Set initial variable for total number of votes cast
+total_number_votes = 0
+
+# Set initial index for the complete list of candidates who received votes
+list_candidates_with_votes = {}
+
+# Read in the CSV file
+with open(election_data_csv, 'r') as csvfile:
+
+     # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+     # Skip row of headers
+    header = next(csvreader)
+
+    # Loop through the data
+    for row in csvreader:
+
+        # Count the total number of votes cast
+        total_number_votes += 1
+
+        # Find all candidate names
+        name_of_candidate = row[2]
+        if name_of_candidate not in list_candidates_with_votes:
+            list_candidates_with_votes[name_of_candidate] = 0
+
+        # Add the votes cast for each candidate
+        list_candidates_with_votes[name_of_candidate] += 1
+
+# Print analysis to console
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_number_votes}")
+print("-------------------------")
+
+# Set variable for the name of the winner
+name_of_winner = ""
+
+# Set initial number of votes for the winner
+votes_for_winner = 0
+
+# Calculate percentage of votes and total number of votes that each candidate won and print them
+for name_of_candidate, votes in list_candidates_with_votes.items():
+    percent = round(votes/total_number_votes*100, 3)
+    print(f"{name_of_candidate}: {percent}% ({votes})")
+    if votes > votes_for_winner:
+        name_of_winner = name_of_candidate
+        votes_for_winner = votes
+
+# Print winner of the election based on popular vote
+print("-------------------------")
+print(f"Winner: {name_of_winner}")
+print("-------------------------")
+
+analysis = os.path.join("analysis/analysis_results.txt")
+
+# Print analysis to text file
+with open(analysis, "w") as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow("Election Results")
+    csvwriter.writerow("----------------------------")
+    csvwriter.writerow(f"Total Votes: {total_number_votes}")
+    csvwriter.writerow("----------------------------")
+
+    # Calculate percentage of votes and total number of votes that each candidate won and print them to text file
+    for name_of_candidate, votes in list_candidates_with_votes.items():
+        percent = round(votes/total_number_votes*100, 3)
+        csvwriter.writerow(f"{name_of_candidate}: {percent}% ({votes})")
+        if votes > votes_for_winner:
+            name_of_winner = name_of_candidate
+            votes_for_winner = votes
+
+    csvwriter.writerow("----------------------------")
+    csvwriter.writerow(f"Winner: {name_of_winner}")
+
+# Print analysis to terminal
+print(analysis)
